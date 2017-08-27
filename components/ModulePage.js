04dddbf20f018f9b0259777f73770c790f6008e8
Edward Shen <6173958+edward-shen@users.noreply.github.com>
2017-08-26 20:20:06 -0400

    migrating to a refresh hoc to avoid calling self.forceUpdate()

--- components/ModulePage.js
+++ components/ModulePage.js
@@ -8,11 +8,18 @@ import {
     List,
     ListItem,
 } from 'react-native-elements';
+import LoadingContainer from 'react-native-loading-container';
 
 export default class ModulePage extends React.Component {
     constructor() {
         super();
-        this._getModuleData();
+        
+        this._getModuleDataPromise = this._getModuleDataPromise.bind(this);
+        this._onReadyAsync = this._onReadyAsync.bind(this);
+        
+        this.state = {
+            moduleData: [],
+        };
         
     }
     
@@ -55,6 +62,36 @@ export default class ModulePage extends React.Component {
         console.log ("testa " + this._moduleData);
     }
     
+    async _getModuleDataPromise() {
+        return await fetch("http://192.168.1.133:8080/remote?action=MODULE_DATA").then(
+            (resp) => {return resp.json()}
+        );  
+        
+        const self = this;
+        try {
+            const address = await AsyncStorage.getItem("MIRROR_IP_ADDRESS");
+            const port = await AsyncStorage.getItem("MIRROR_PORT");
+            if (address !== null) {
+                // Fetch returns a promise, which contains our json data
+                // the json data returned is actually still a promise, so we need to use another .then()
+                // properly parse it
+                return await fetch("http://" + address + ":" + (port ? port !== null : "8080") + "/remote?action=MODULE_DATA").json();
+            } else {
+                console.log("address is null!")
+            }
+        } catch (err) {
+            console.log(err);
+        }
+        
+    }
+    
+    async _onReadyAsync( {moduleData: data} ) {
+        const self = this;
+        return new Promise((resolve) => {
+            self.setState({ moduleData: data }, resolve);
+        });
+        
+    }
     render() {
         const self = this;
         return (
@@ -65,18 +102,23 @@ export default class ModulePage extends React.Component {
                     onPress = { () => this._getModuleData() }
                     title = "Refresh Module Data" />
                     
-                <List>
-                    {
-                        self._moduleData.map((item, i) => (
-                            <ListItem
-                                key={i}
-                                title={item.name}
-                                subtitle={item.identifier} 
-                            />
-                        ))
-                    }
-                </List>
-                </ScrollView>
+                <LoadingContainer
+                    onLoadStartAsync = { this._getModuleDataPromise }
+                    onReadyAsync = { this._onReadyAsync } >
+                    
+                    <List>
+                        {
+                            self.state.moduleData.map((item, i) => (
+                                <ListItem
+                                    key={item.identifier}
+                                    title={item.name}
+                                    subtitle={item.identifier} 
+                                />
+                            ))
+                        }
+                    </List>
+                </LoadingContainer>
+            </ScrollView>
         );
     }
 }
